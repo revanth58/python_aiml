c3dbcb8a18fa95b6dcf1f6adb8c6b3e5bc50db17
revanth58 <revanthk7777@gmail.com>
2026-04-16 07:36:40 +0530

    sfverfvre

--- programs/list.py
+++ programs/list.py
@@ -20,4 +20,32 @@ def print_list(head):
 		print(x.value)
 		x = x.next
 
-print_list(n2)
+#print_list(n1)
+
+def arrtolist(nums):
+	head = None
+	prev = None
+
+	for x in nums :
+		p = Node(x)
+		if head is None:
+			head = p
+		else :
+			prev.next = p
+
+		prev = p
+
+	return head
+
+z = arrtolist([123,345,67,3,23,6,7,4,6,7,3,125,333,4])
+
+def return_last(head):
+	if head is None:
+		print("invalid : empty list")
+		return None
+	x = head
+	while x.next is not None:
+		x = x.next
+	return x
+
+print(return_last(z).value)
